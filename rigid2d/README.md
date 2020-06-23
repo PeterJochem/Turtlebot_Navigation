23905e00c06126058436b9e73e592cc41177b3f0
Peter Jochem <peterpennrichjochem@gmail.com>
2020-06-22 19:17:05 -0500

    Sketched out the testing methods

--- rigid2d/README.md
+++ rigid2d/README.md
@@ -3,3 +3,8 @@
 # Compiling 
 I am using Eigen and it is installed at /usr/include/eigen3 so I need to link with -I 
 ```g++ -Wall -Wextra -g -std=c++17 -I /usr/include/eigen3 -o rigid2d_test main.cpp rigid2d.cpp```
+
+
+
+# Describe the testing 
+Describe the folders of test files
--- rigid2d/main.cpp
+++ rigid2d/main.cpp
@@ -2,50 +2,90 @@
  */
 #include "rigid2d.hpp"
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main() {
+
+/* Given T_ab and T_bc, compute T_ac
+ */
+rigid2d::Transform2D testTransforms(rigid2d::Transform2D T_ab, rigid2d::Transform2D T_bc) {
+	
+	rigid2d::Transform2D T_ac = T_ab * T_bc;
+	std::cout << T_ac << std::endl;	
+
+	// Write T_ac to the output file
+
+	return T_ac;
+}
+
+
+
+
+/* The input will be a scalar indicating which testing files to use
+ * So if argv[3] = N, then we will use the Nth set of testing files
+ */
+int main(int argc, char *argv[]) {
 	
 	// For testing, use this
 	double pi = 3.14159265358979;
-
-	rigid2d::Vector2D myVector1;
-	myVector1.x = 1.0;
-	myVector1.y = 2.0;
-
-	rigid2d::Vector2D myVector2;
-        myVector2.x = -400.0;
-        myVector2.y = -20.0;
-
-	// Test each of the three constructors
-	//rigid2d::Transform2D tf3; // = rigid2d::Transform2D();	
 	
-	rigid2d::Transform2D tf2 = rigid2d::Transform2D(myVector1);
-	
+	// The output files are at test/output/testN.txt	
+	// The input files are at test/input/testN.txt
 		
-	// rigid2d::Transform2D tf3 = rigid2d::Transform2D(0);	
-	// std::cout << tf3(myVector1);
-	// std::cout << tf3.getTf() << std::endl; 
-				
-	//std::cin >> tf3;		
-	//std::cout << tf3;
-
-	// rigid2d::Twist2D myTwist = rigid2d::Twist2D(0.0, 1.4, 2.0);	
-	// std::cout << myTwist;
-
-	rigid2d::Twist2D myTwist; // = rigid2d::Twist2D(0.0, 1.0, 2.0);   
-        //std::cin >> myTwist;	
-	//std::cout << myTwist;
+	std::cout << atof( argv[1] ) << std::endl;
 	
-	// Convert the twist from tf3 to the new frame 
-	// std::cout << tf3(myTwist, tf2);
+	char inputFileName[] = "test/inputs/testX.txt";
+	int testSet = atof( argv[1] );
+		
+	inputFileName[16] = *argv[1];
+	std::cout << inputFileName << std::endl;  
 	
-	// std::cout << tf2.adjoint() << std::endl;		
+	// Re-direct stdin to the input file
+	std::ifstream in(inputFileName);
+	std::streambuf *cinbuf = std::cin.rdbuf(); //save old buf
+    	std::cin.rdbuf(in.rdbuf()); //redirect std::cin to in.txt!
 	
-	// Test the skew-symmetric method
-	// std::cout << rigid2d::skew_sym(myVector1) << std::endl;   	
+	double i = -1;
+	std::cin >> i;
+	std::cout << i << std::endl;
 
-	std::cout << tf2.adjoint() << std::endl;
+	rigid2d::Transform2D T_ab;
+	std::cout << "Enter T_ab \n";	
+	std::cin >> T_ab;	
 
+	rigid2d::Transform2D T_bc;
+        std::cout << "Enter T_bc \n";
+        std::cin >> T_bc;
+	
+
+	testTransforms(T_ab, T_bc);
+			
+	/*
+	double x = -1.0;
+	double y = -1.0;
+	
+	char frame = 'A';
+
+	std::cout << "Enter a vector v \n";	
+	
+	std::cout << "Enter {A, B, C} indicating the frame in which the vector is defined \n";
+	std::cin >> frame;
+
+	std::cout << "Enter a vector's x component \n";
+	std::cin >> x;
+	
+	std::cout << "Enter a vector's y component \n";
+	std::cin >> y;
+
+	rigid2d::Vector2D V1;
+	V1.x = x;
+	V1.y = y;	
+	
+	std::cout << "The vector is ";
+	std::cout << "(" << V1.x << ", " << V1.y << ") and is defined in the " << frame << " frame \n";
+
+	// Output the 	
+	*/ 
 
 	return 1;
 }
