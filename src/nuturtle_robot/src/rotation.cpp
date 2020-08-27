020b3384fa4008df7da441100a078a4b95989efd
Peter Jochem <peterpennrichjochem@gmail.com>
2020-08-27 16:45:59 -0500

    The turtle interface node seems to work and is passing the rostests. I wrote the launch file to do the basic testing of the robot rotating! Pretty exciting day. The robot is a moving!

--- src/nuturtle_robot/src/rotation.cpp
+++ src/nuturtle_robot/src/rotation.cpp
@@ -1,23 +1,34 @@
 #include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include <sensor_msgs/JointState.h>
+#include <sstream>
+#include <stdlib.h>
 #include "std_msgs/String.h"
 #include <sstream>
+#include "nuturtlebot/WheelCommands.h"
+#include "nuturtlebot/SensorData.h"
+#include "rigid2d/rigid2d.hpp"
+#include "rigid2d/diff_drive.hpp"
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
+
 int main(int argc, char **argv) {
   
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "rotation");
 
   ros::NodeHandle n;
-  ros::Publisher cmd_vel_pub = n.advertise<std_:x
-	  msgs::String>("chatter", 1000);
+  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
 
   ros::Rate loop_rate(10);
 
   while (ros::ok()) {
-    ros::spinOnce();
-    loop_rate.sleep();
+     
+     geometry_msgs::Twist newTwist;
+     newTwist.angular.z = 1.0;
+   
+     cmd_vel_pub.publish(newTwist); 
+
+     ros::spinOnce();
+     loop_rate.sleep();
   }
 
   return 0;
