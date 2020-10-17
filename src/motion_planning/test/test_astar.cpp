54f869202a9155901ed7ab9c3ec8efcf41045d2c
Peter Jochem <peterpennrichjochem@gmail.com>
2020-10-17 14:08:24 -0500

    I setup the ROS CmakeLists and package.xml files so that I can test the A_Star_Planner independently of the RVIZ/ROS with gtest

--- src/motion_planning/test/test_astar.cpp
+++ src/motion_planning/test/test_astar.cpp
@@ -1,9 +1,14 @@
+#include "motion_planning/A_Star_Planner.hpp"
 #include <gtest/gtest.h>
-//#include <a_star.hpp>
+
 
 TEST(myName, myOtherName) {
+	
+	std::cout << "WE ARE HERE" << std::endl;
+	
+	int map [5] = {16, 2, 77, 40, 12071}; 
 
-
+	A_Star_Planner myPlanner = A_Star_Planner(nullptr, 1, 1, 1);
 }
 
 
