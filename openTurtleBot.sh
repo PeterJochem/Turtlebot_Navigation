c5adf178e84f68fa3b1ace035b37eff001d3e712
PeterJochem <peterjochem2020@u.northwestern.edu>
2020-11-08 18:40:25 -0600

    I added a desktop file for starting terminal windows for Turtlebot development. This allows me to create three terminal tabs all at the catkin_ws and start developing faster

--- openTurtleBot.sh
+++ openTurtleBot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /home/peter/Desktop/Turtlebot/catkin_ws
+cd /home/peterjochem/Desktop/catkin_ws
 source devel/setup.bash # The children window's inherit this too :)
 gnome-terminal --tab --tab --tab # Open a new tab
 
